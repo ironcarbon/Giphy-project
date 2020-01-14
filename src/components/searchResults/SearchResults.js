6164f62d87c5e55deb373c1e96b01939c76ba48b
ipek <ipekcivicakan@gmail.com>
2020-01-14 16:47:27 -0500

    routing structure organized

---
+++ src/components/searchResults/SearchResults.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+
+
+class SearchResults extends React.Component {
+    render() {
+        return (
+            <Fragment>
+                <p>Search Results</p>
+            </Fragment>
+        )
+    }
+
+}
+
+export default SearchResults;

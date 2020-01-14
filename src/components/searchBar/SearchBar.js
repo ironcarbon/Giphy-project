e8074a08e3156c3e163b0f0aa4720a7708e75fb5
ipek <ipekcivicakan@gmail.com>
2020-01-14 18:38:25 -0500

    SearchBar functionality done

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,16 +1,39 @@
 import React, { Fragment } from 'react';
-import styles from './SearchBar.module.css';
-
+import { withRouter } from 'react-router-dom';
 
 class SearchBar extends React.Component {
+    state = { searchedKeyword: "", error: "" };
+
+    inputHandler = (e) => {
+        this.setState({ searchedKeyword: e.target.value })
+    }
+
+    formHandler = (e) => {
+        e.preventDefault();
+        if (!this.state.searchedKeyword) {
+            this.setState({ error: 'Please search a giph' })
+        } else {
+            console.log(this.props.history.push({
+                pathname: `/search/${this.state.searchedKeyword}`,
+                state: {
+                    searchedKeyword: this.state.searchedKeyword
+                }
+            }))
+        }
+
+    }
     render() {
         return (
+
             <Fragment>
-                <p>SearchBar</p>
+                {this.state.error ? <div>{this.state.error}</div> : ""}
+                <form onSubmit={this.formHandler}>
+                    <input type="text" onChange={this.inputHandler} placeholder='Search a Giph' />
+                    <input type="submit" value="Search" />
+                </form>
             </Fragment>
         )
     }
-
 }
 
-export default SearchBar;
+export default withRouter(SearchBar); 

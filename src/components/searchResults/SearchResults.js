74f4b74b23658aa5d6fb9836f356aeff9c5bce9a
ipek <ipekcivicakan@gmail.com>
2020-01-14 19:08:17 -0500

    TrendingGif component functionality done

--- src/components/searchResults/SearchResults.js
+++ src/components/searchResults/SearchResults.js
@@ -1,24 +1,3 @@
-// import React, { Fragment } from 'react';
-
-
-// class SearchResults extends React.Component {
-//     render() {
-//         return (
-//             <Fragment>
-//                 <p>Search Results</p>
-//             </Fragment>
-//         )
-//     }
-
-// }
-
-// export default SearchResults;
-
-
-
-
-
-
 import React, { Fragment } from 'react';
 import { giphyApiSearchRequest } from '../../utils/api';
 

0ca901f1206cc4b039dba0e8ad417d1773ff9237
ipek <ipekcivicakan@gmail.com>
2020-01-15 02:52:48 -0500

    refactored no result logic

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,24 +1,27 @@
 import React, { Fragment } from 'react';
 import { withRouter } from 'react-router-dom';
 import styles from './SearchBar.module.css';
-import search from './search.png';
+// import search from './search.png';
 
 class SearchBar extends React.Component {
-    state = { searchedKeyword: "", error: "" };
+    state = { searchedKeyword: "", error: null };
 
     inputHandler = (e) => {
-        this.setState({ searchedKeyword: e.target.value })
+        const cleanStr = e.target.value.replace(/[^a-zA-Z]/g, "")
+        this.setState({ searchedKeyword: cleanStr })
     }
 
     formHandler = (e) => {
         e.preventDefault();
         if (!this.state.searchedKeyword) {
             this.setState({
-                error: (<div className={styles.errorContainer}>< div className={styles.error}>
+                error: (<div className={styles.errorContainer}>
                     <img src="https://media.giphy.com/media/1QGRJ9cOTbh5K/giphy.gif" />
-                </div><p>Please Search a Gif</p></div>)
+                    <p>Please Search a Gif</p></div>)
             })
         } else {
+            this.setState({ error: null })
+
             this.props.history.push({
                 pathname: `/search/${this.state.searchedKeyword}`,
                 state: {
@@ -32,15 +35,17 @@ class SearchBar extends React.Component {
         return (
 
             <Fragment>
-                {this.state.error ? <div>{this.state.error}</div> : ""}
+
                 <div className={styles.container}>
                     <form className={styles.form} onSubmit={this.formHandler}>
                         <div className={styles.formContainer}>
-                            <input type="text" className={styles.search} onChange={this.inputHandler} placeholder='Search a Gif' required />
-                            <img src={search} alt="search" className={styles.image} onClick={this.formHandler} />
+                            <input type="text" className={styles.search} onChange={this.inputHandler} required placeholder='Search a Gif' />
+                            <button type="submit" className={styles.submitButton} />
+                            {/* <img src={search} alt="search" className={styles.image} onClick={this.formHandler} /> */}
                         </div>
                     </form>
                 </div>
+                {this.state.error ? <Fragment>{this.state.error}</Fragment> : ""}
             </Fragment>
         )
     }
